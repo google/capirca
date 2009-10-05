e36c4243032be1dd430cfc975d4dd875769185f6
watson <watson@506f5edc-932d-11de-9897-6f94e350a523>
2009-10-05 16:41:07 +0000

    update license headers in source files

--- trunk/aclcheck.py
+++ trunk/aclcheck.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python2.4
 #
-# Copyright 2008 Google Inc. All Rights Reserved.
-# $Id: //depot/google3/ops/security/lib/aclcheck.py#13 $
-# $Date: 2009/04/28 $
+# Copyright 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 
 """Check where hosts, ports and protocols are matched in a NAC policy.
