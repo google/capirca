3bba94279c5141ef55d455796950583cdf8fa699
peter.moody <peter.moody@506f5edc-932d-11de-9897-6f94e350a523>
2009-10-20 01:42:51 +0000

    + move parser stuff outside of functions (more in line with google    style guide)  + make more informative errors for things like no policies/definitions

--- aclgen.py
+++ aclgen.py
@@ -19,6 +19,22 @@ from lib import juniper
 
 output_policy_dict = {}
 
+parser = OptionParser()
+parser.add_option('-d', '--def',
+                  dest='definitions',
+                  help='defintions directory')
+parser.add_option('-o', '--output_directory',
+                  dest='output_directory',
+                  default='filter', help='output directory')
+parser.add_option('-p', '--pol',
+                  dest='policy',
+                  help='policy file')
+parser.add_option('', '--poldir',
+                  dest='policies',
+                  help='policy directory')
+(options, args) = parser.parse_args()
+
+
 def render_policy(pol, input_file, output_directory):
   output_dir = os.path.join(os.path.dirname(os.path.dirname(input_file)),
                             output_directory)
@@ -36,10 +52,10 @@ def output_policies():
     if output:
       output.write(output_policy_dict[output_file])
 
-def main(options):
+def main():
   # first, load our naming
   if not options.definitions:
-    return
+    parser.error('no definitions supplied')
   defs = naming.Naming(options.definitions)
   if not defs:
     print 'problem loading definitions'
@@ -49,7 +65,7 @@ def main(options):
   # been given both and error if so. if we have a poldir, loop through the
   # directory, looking for policy files to render.
   if not options.policy:
-    return
+    parser.error('no policy supplied')
   pol = policy.ParsePolicy(open(options.policy).read(), defs)
 
   for header in pol.headers:
@@ -67,17 +83,7 @@ def main(options):
       print 'rendering cisco'
       render_policy(cisco.Cisco(pol), options.policy,
                     options.output_directory)
-
     output_policies()
 
 if __name__ == '__main__':
-  parser = OptionParser()
-  parser.add_option('-d', '--def', dest='definitions',
-                    help='defintions directory')
-  parser.add_option('-o', '--output_directory', dest='output_directory',
-                    default='filter', help='output directory')
-  parser.add_option('-p', '--pol', dest='policy', help='policy file')
-  parser.add_option('', '--poldir', dest='policies', help='policy directory')
-  (options, args ) = parser.parse_args()
-  
-  main(options)
+  main()
