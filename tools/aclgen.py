99b4b87d02eb00af31bce3364504e208cc774458
peter.moody <peter.moody@506f5edc-932d-11de-9897-6f94e350a523>
2009-10-20 01:42:11 +0000

    + beginnings of the aclgen script.

---
+++ tools/aclgen.py
@@ -0,0 +1,23 @@
+#!/Usr/bin/env python
+#
+# this is the beginnings of the tool which will render policy
+# files into usable iptables tables, cisco access lists or
+# juniper firewall filters.
+
+# system imports
+import sys
+
+# compiler imports
+from lib import naming
+from lib import policy
+
+# renderer imports
+from lib import cisco
+from lib import iptables
+from lib import juniper
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
