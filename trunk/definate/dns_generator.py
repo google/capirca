18ea7c9ad8b88eec98c984f8b53f4d80e85fe896
msu@google.com <msu@google.com@506f5edc-932d-11de-9897-6f94e350a523>
2012-12-07 09:39:10 +0000

    Opensource Definate which allows to autogenerate network definitions under Capirca

---
+++ trunk/definate/dns_generator.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generator for DNS based network definitions."""
+
+__author__ = 'msu@google.com (Martin Suess)'
+
+
+import logging
+import socket
+
+from third_party import ipaddr
+import generator
+
+
+class DnsGeneratorError(Exception):
+  """Exception to use when DnsGenerator fails."""
+
+
+class DnsGenerator(generator.Generator):
+  """Generator implementation for network definitions based on DNS."""
+
+  SUPPORTED_TYPES = ['A', 'AAAA']
+
+  def GenerateDefinition(self, config, unused_global_config):
+    """Generates a list of all nodes in a network definition.
+
+    This method basically processes all the configuration which is
+    hierarchically below "networks" in the "definitions" section in the
+    configuration file to generate a list of all nodes in that definition.
+
+    Args:
+      config: YAML configuration structure (dictionaries, lists and strings)
+        representing the "networks" section in "definitions" of the
+        configuration file.
+      unused_global_config: YAML configuration structure (dictionaries, lists
+        and strings) representing the "global" section of the configuration
+        file.
+
+    Returns:
+      Tuples of IPNetwork objects and string comments representing all the nodes
+      in one definition.
+
+    Raises:
+      DefinateConfigError: The configuration is not well formed.
+      DnsGeneratorError: There is a problem generating the output.
+    """
+    nodes = []
+    yaml_structure = {
+        'names': ['str'],
+        'types': ['str'],
+        }
+    for network in config:
+      self._yaml_validator.CheckConfiguration(network, yaml_structure)
+      for typ in network['types']:
+        if typ not in self.SUPPORTED_TYPES:
+          raise DnsGeneratorError('Unsupported DNS type found: %s' % typ)
+      for name in network['names']:
+        addr_list = socket.getaddrinfo(name, None)
+        for family, _, _, _, sockaddr in addr_list:
+          ip_addr = None
+          if family == socket.AF_INET and 'A' in network['types']:
+            # sockaddr = (address, port)
+            ip_addr = ipaddr.IPv4Network(sockaddr[0])
+          elif family == socket.AF_INET6 and 'AAAA' in network['types']:
+            # sockaddr = (address, port, flow info, scope id)
+            ip_addr = ipaddr.IPv6Network(sockaddr[0])
+          else:
+            logging.debug('Skipping unknown AF \'%d\' for: %s', family, name)
+          if ip_addr:
+            nodes.append((ip_addr, name))
+    return nodes
---
+++ trunk/definate/file_filter.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module that holds all file-level filter classes of Definate."""
+
+__author__ = 'msu@google.com (Martin Suess)'
+
+
+import logging
+
+
+class Error(Exception):
+  """Base error class."""
+
+
+class FileError(Error):
+  """Exception to use when file handling files."""
+
+
+class Container(object):
+  """Container class to hold all information to be passed between filters."""
+
+  def __init__(self, lines=None, relative_path='', absolute_path=''):
+    """Initializer.
+
+    Args:
+      lines: Optional list of strings which will be added as lines.
+        E.g. the file header can be added here directly.
+      relative_path: Optional string to specify the path of the file relative to
+        the location of the definition directory (e.g. 'AUTOGEN.net').
+      absolute_path: Optional string to specify the absolute path of the local
+        file to be written (e.g. '/tmp/AUTOGEN.net') or if a SCM software is
+        used it can refer to the full path there
+        (e.g. '//depot/def/AUTOGEN.net').
+    """
+    self.lines = lines if lines else []
+    self.absolute_path = absolute_path
+    self.relative_path = relative_path
+
+
+class FileFilter(object):
+  """Abstract class defining the interface for the filter chain objects."""
+
+  def Filter(self, container, args):
+    """Interface to filter or modify data passed into it.
+
+    Args:
+      container: Container object which holds all information for one definition
+        file. See Container class for details.
+      args: Dictionary of arguments depending on the actual filter in use.
+
+    Raises:
+      NotImplementedError: In any case since this is not implemented an needs
+        to be defined by subclasses.
+    """
+    raise NotImplementedError(
+        'This is an interface only. Implemented by subclasses.')
+
+
+class PrintFilter(FileFilter):
+  """FileFilter implementation which simply logs the file content."""
+
+  def Filter(self, container, unused_args):
+    """Filter method that prints the content of the file to stdout.
+
+    Args:
+      container: Container object which holds all information for one definition
+        file. See Container class for details.
+      unused_args: No extra arguments required by this filter implementation.
+
+    Returns:
+      Container object that has been passed in.
+    """
+    print '# File "%s"' % container.absolute_path
+    print '\n'.join(container.lines)
+    return container
+
+
+class WriteFileFilter(FileFilter):
+  """FileFilter implementation which writes the content into a file."""
+
+  def Filter(self, container, unused_args):
+    """Filter method that writes the content of the file into a file.
+
+    Args:
+      container: Container object which holds all information for one definition
+        file. See Container class for details.
+      unused_args: No extra arguments required by this filter implementation.
+
+    Returns:
+      Container object that has been passed in.
+    """
+    try:
+      f = file(container.absolute_path, 'w')
+    except IOError as e:
+      raise FileError('File "%s" could not be opened: %s' % (
+          container.absolute_path, e))
+
+    try:
+      f.write('\n'.join(container.lines))
+    except IOError as e:
+      raise FileError('File "%s" could not be written: %s' % (
+          container.absolute_path, e))
+    else:
+      f.close()
+
+    logging.info('Wrote file: %s', container.absolute_path)
+    return container
