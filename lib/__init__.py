ed180b2ba7c8c4d355dd829521be9a2194c44265
Jeff Zohrab <jzohrab@gmail.com>
2016-05-31 11:11:38 -0400

    lib should log to NullHandler by default.
    Capirca classes could potentially be exposed as a library for use via Ansible
    or similar.  Libraries should use NullHandler for loggers by default.  ref
    http://docs.python-guide.org/en/latest/writing/logging/ for discussion.

--- lib/__init__.py
+++ lib/__init__.py
@@ -31,3 +31,14 @@ __all__ = ['naming', 'yamlnaming', 'policy', 'cisco', 'cisconx', 'juniper', 'ipt
            'packetfilter', 'port', 'speedway', 'gce']
 
 __author__ = 'Paul (Tony) Watson (watson@gmail.com / watson@google.com)'
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
