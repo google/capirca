527570a3cd7ed8193e64150050fdb5ee42bd3c73
Jeff Zohrab <jzohrab@gmail.com>
2016-06-02 17:25:39 -0400

    Disabling some tests.
    Not clear if this is the best idea or not.  These tests were added to ensure
    that the wrapper script would continue to work for old clients, but with these
    latest changes from upstream google Capirca master, things are no longer
    backwards-compatible anyway.

--- test/integration/test_aclgen.py
+++ test/integration/test_aclgen.py
@@ -22,7 +22,6 @@ from cStringIO import StringIO
 import filecmp
 
 import aclgen
-from aclgen import AclGen
 from lib import policy
 
 
@@ -84,33 +83,35 @@ writing ./filters/sample_srx.srx
     self.assertEquals(expected_output, self.iobuff.getvalue())
 
 
-class AclGen_filter_name_scenarios(unittest.TestCase):
-  """Ensure the output directory structure mirrors the input correctly.
+# (Temporarily?) disabling.
+#
+# class AclGen_filter_name_scenarios(unittest.TestCase):
+#   """Ensure the output directory structure mirrors the input correctly.
 
-  See aclgen.filter_name for notes."""
+#   See aclgen.filter_name for notes."""
 
-  def test_file_in_base_directory_is_output_to_output_dir(self):
-    s = AclGen.filter_name('/policy', '/policy/x.txt', '.out', '/output')
-    self.assertEqual(s, '/output/x.out')
+#   def test_file_in_base_directory_is_output_to_output_dir(self):
+#     s = AclGen.filter_name('/policy', '/policy/x.txt', '.out', '/output')
+#     self.assertEqual(s, '/output/x.out')
 
-  def test_file_in_subdirectory_is_output_to_subdirectory(self):
-    s = AclGen.filter_name('/policy', '/policy/subdir/x.txt', '.out', '/output')
-    self.assertEqual(s, '/output/subdir/x.out')
+#   def test_file_in_subdirectory_is_output_to_subdirectory(self):
+#     s = AclGen.filter_name('/policy', '/policy/subdir/x.txt', '.out', '/output')
+#     self.assertEqual(s, '/output/subdir/x.out')
 
-  def test_file_in_nested_subdir(self):
-    s = AclGen.filter_name('/policy', '/policy/sub/dir/x.txt', '.out', '/output/here')
-    self.assertEqual(s, '/output/here/sub/dir/x.out')
+#   def test_file_in_nested_subdir(self):
+#     s = AclGen.filter_name('/policy', '/policy/sub/dir/x.txt', '.out', '/output/here')
+#     self.assertEqual(s, '/output/here/sub/dir/x.out')
 
-  def test_relative_directory_structure_mirrored(self):
-    s = AclGen.filter_name('../policy', '../policy/subdir/x.txt', '.out', '/output')
-    self.assertEqual(s, '/output/subdir/x.out')
+#   def test_relative_directory_structure_mirrored(self):
+#     s = AclGen.filter_name('../policy', '../policy/subdir/x.txt', '.out', '/output')
+#     self.assertEqual(s, '/output/subdir/x.out')
 
-  def test_empty_source_dir_ok(self):
-    s = AclGen.filter_name('', 'subdir/x.txt', '.out', '/output')
-    self.assertEqual(s, '/output/subdir/x.out')
+#   def test_empty_source_dir_ok(self):
+#     s = AclGen.filter_name('', 'subdir/x.txt', '.out', '/output')
+#     self.assertEqual(s, '/output/subdir/x.out')
 
-  def test_base_dir_must_match_start_of_source_file(self):
-    self.assertRaises(ValueError, AclGen.filter_name, 'A', 'B/C', 'suff', 'O')
+#   def test_base_dir_must_match_start_of_source_file(self):
+#     self.assertRaises(ValueError, AclGen.filter_name, 'A', 'B/C', 'suff', 'O')
 
 
 class AclGen_Characterization_Test_Base(unittest.TestCase):
@@ -165,60 +166,63 @@ class AclGen_Characterization_Tests(AclGen_Characterization_Test_Base):
     aclgen.main(['-d', def_dir, '--poldir', pol_dir, '-o', self.output_dir, '--debug'])
     # Sanity check to ensure runs only - if we reach here assume ok.
 
-  def test_can_make_direct_API_call_to_load_and_render(self):
-    """Existing clients may have been making calls directly
-    to aclgen.load_and_render, double-checking it still works."""
-    def_dir, pol_dir, expected_dir = map(self.testpath, ('def', 'policies', 'filters_expected'))
-    aclgen.load_and_render(pol_dir, def_dir, False, 2, self.output_dir)
+  # Old tests, disabling.  Existing clients are perhaps using the wrapper as-is.
+  #
+  # def test_can_make_direct_API_call_to_load_and_render(self):
+  #   """Existing clients may have been making calls directly
+  #   to aclgen.load_and_render, double-checking it still works."""
+  #   def_dir, pol_dir, expected_dir = map(self.testpath, ('def', 'policies', 'filters_expected'))
+  #   aclgen.load_and_render(pol_dir, def_dir, False, 2, self.output_dir)
 
-    dircmp = filecmp.dircmp(self.output_dir, expected_dir)
-    self.assertEquals([], dircmp.left_only, 'missing {0} in filters_expected'.format(dircmp.left_only))
-    self.assertEquals([], dircmp.right_only, 'missing {0} in filters_actual'.format(dircmp.right_only))
-    self.assertEquals([], dircmp.diff_files)
+  #   dircmp = filecmp.dircmp(self.output_dir, expected_dir)
+  #   self.assertEquals([], dircmp.left_only, 'missing {0} in filters_expected'.format(dircmp.left_only))
+  #   self.assertEquals([], dircmp.right_only, 'missing {0} in filters_actual'.format(dircmp.right_only))
+  #   self.assertEquals([], dircmp.diff_files)
 
-  def test_can_make_direct_API_call_to_render_filters(self):
-    """Existing clients may have been making calls directly
-    to aclgen.render_filters, double-checking it still works."""
-    def_dir, pol_dir, expected_dir = map(self.testpath, ('def', 'policies', 'filters_expected'))
-    src = os.path.join(pol_dir, 'sample_cisco_lab.pol')
-    aclgen.render_filters(src, def_dir, False, 2, self.output_dir)
-    # If we get here, assume all is OK.
+  # def test_can_make_direct_API_call_to_render_filters(self):
+  #   """Existing clients may have been making calls directly
+  #   to aclgen.render_filters, double-checking it still works."""
+  #   def_dir, pol_dir, expected_dir = map(self.testpath, ('def', 'policies', 'filters_expected'))
+  #   src = os.path.join(pol_dir, 'sample_cisco_lab.pol')
+  #   aclgen.render_filters(src, def_dir, False, 2, self.output_dir)
+  #   # If we get here, assume all is OK.
 
 
-class AclGen_Create_filter_for_target(AclGen_Characterization_Test_Base):
-  """Given a policy, generate filter text for a particular target."""
+# Old test, disabling.
+# class AclGen_Create_filter_for_target(AclGen_Characterization_Test_Base):
+#   """Given a policy, generate filter text for a particular target."""
 
-  def get_acl_gen(self):
-    return AclGen(policy_directory = self.testpath('policies'),
-                  definitions_directory = self.testpath('def'),
-                  output_directory = self.testpath('filters_expected'))
+#   def get_acl_gen(self):
+#     return AclGen(policy_directory = self.testpath('policies'),
+#                   definitions_directory = self.testpath('def'),
+#                   output_directory = self.testpath('filters_expected'))
 
-  def test_can_generate_filter_from_policy_for_specified_platform(self):
-    src = self.testpath('policies', 'sample_cisco_lab.pol')
-    definitions = self.testpath('def')
-    a = self.get_acl_gen()
-    fw = a.create_filter_for_platform('cisco', src)
-    actual_filter = str(fw)
-    with open(self.testpath('filters_expected', 'sample_cisco_lab.acl'), 'r') as f:
-      expected_filter = f.read()
+#   def test_can_generate_filter_from_policy_for_specified_platform(self):
+#     src = self.testpath('policies', 'sample_cisco_lab.pol')
+#     definitions = self.testpath('def')
+#     a = self.get_acl_gen()
+#     fw = a.create_filter_for_platform('cisco', src)
+#     actual_filter = str(fw)
+#     with open(self.testpath('filters_expected', 'sample_cisco_lab.acl'), 'r') as f:
+#       expected_filter = f.read()
 
-    # If different, save for manual check.
-    if (actual_filter != expected_filter):
-      with open(self.testpath('filters_actual', 'sample_cisco_lab.acl'), 'w') as f:
-        f.write(actual_filter)
-    self.assertEqual(actual_filter, expected_filter)
+#     # If different, save for manual check.
+#     if (actual_filter != expected_filter):
+#       with open(self.testpath('filters_actual', 'sample_cisco_lab.acl'), 'w') as f:
+#         f.write(actual_filter)
+#     self.assertEqual(actual_filter, expected_filter)
 
-  def test_generating_filter_for_missing_platform_throws(self):
-    a = self.get_acl_gen()
-    with self.assertRaises(policy.PolicyTargetPlatformInvalidError):
-      a.create_filter_for_platform('missing', '')
+#   def test_generating_filter_for_missing_platform_throws(self):
+#     a = self.get_acl_gen()
+#     with self.assertRaises(policy.PolicyTargetPlatformInvalidError):
+#       a.create_filter_for_platform('missing', '')
 
-  def test_cannot_generate_filter_from_policy_for_platform_different_from_policy_header(self):
-    a = self.get_acl_gen()
-    src = self.testpath('policies', 'sample_cisco_lab.pol')
-    definitions = self.testpath('def')
-    with self.assertRaises(policy.PolicyTargetPlatformInvalidError):
-      a.create_filter_for_platform('juniper', src)
+#   def test_cannot_generate_filter_from_policy_for_platform_different_from_policy_header(self):
+#     a = self.get_acl_gen()
+#     src = self.testpath('policies', 'sample_cisco_lab.pol')
+#     definitions = self.testpath('def')
+#     with self.assertRaises(policy.PolicyTargetPlatformInvalidError):
+#       a.create_filter_for_platform('juniper', src)
 
 
 def main():
