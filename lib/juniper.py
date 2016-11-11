9138230224bf3beb51e788702eead5a86277419c
Ryan Tierney <rtierney@fb.com>
2016-11-11 13:23:54 -0800

    add flexible-match-range functionality to juniper platform

--- lib/juniper.py
+++ lib/juniper.py
@@ -271,6 +271,7 @@ class Term(aclgenerator.Term):
                           self.term.destination_port or
                           self.term.destination_prefix or
                           self.term.ether_type or
+                          self.term.flexible_match_range or
                           self.term.forwarding_class or
                           self.term.hop_limit or
                           self.term.next_ip or
@@ -484,6 +485,13 @@ class Term(aclgenerator.Term):
         # Only generate a hop-limit if inet6, inet4 has not hop-limit.
         if self.term_type == 'inet6':
           config.Append('hop-limit %s;' % (self.term.hop_limit))
+
+      # flexible-match
+      if self.term.flexible_match_range:
+        config.Append('flexible-match-range {')
+        for fm_opt in self.term.flexible_match_range:
+          config.Append('%s %s;' % (fm_opt[0], fm_opt[1]))
+        config.Append('}')
 
       config.Append('}')  # end from { ... }
 
@@ -771,6 +779,7 @@ class Juniper(aclgenerator.ACLGenerator):
                          'dscp_match',
                          'dscp_set',
                          'ether_type',
+                         'flexible_match_range',
                          'forwarding_class',
                          'fragment_offset',
                          'hop_limit',
