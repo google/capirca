50ade1f923603e130251eac0301d06c2a16516e9
Rob Ankeny <robankeny@google.com>
2016-11-28 20:12:30 -0800

    Fixing integration test that were missed

--- tests/integration/aclgen_test.py
+++ tests/integration/aclgen_test.py
@@ -158,7 +158,6 @@ class AclGenArgumentsTests(AclGenCharacterizationTestBase):
 class AclGenCharacterizationTests(AclGenCharacterizationTestBase):
 
   def test_characterization(self):
-    # import pudb;pudb.set_trace()
     def_dir, pol_dir, expected_dir = map(
         self.dirpath, ('def', 'policies', 'filters_expected'))
     args = [
