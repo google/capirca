fa14b41f556e3995a372b473b11b1760947ee572
robankeny <robankeny@google.com>
2019-05-03 11:20:56 -0700

    Incrementing version in setup.py
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=246542462

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='capirca',
-    version='1.121',
+    version='1.122',
     description='Capirca',
     license='Apache License, Version 2.0',
     url='https://github.com/google/capirca/',
