35b1722364ad2e9ec613ec2037f5f324dd5bde05
Natalia Maximo <nmaximo@google.com>
2020-07-10 10:48:36 -0400

    chg: doc: added comment strings for explaining why pylint disables were done

--- capirca/lib/nacaddr.py
+++ capirca/lib/nacaddr.py
@@ -323,7 +323,10 @@ def _CollapseAddrListInternal(addresses, complements_by_network):
         prev_addr.AddComment(addr.text)
       elif (prev_addr.version == addr.version and
             prev_addr.prefixlen == addr.prefixlen and
+            # It's faster to compare integers than IP objects
             prev_addr.broadcast_address._ip + 1 == addr.network_address._ip and  # pylint disable=protected-access
+            # Generating Super is relatively intensive compared to doing bit
+            # operations
             (prev_addr.netmask._ip << 1) & prev_addr.network_address._ip ==      # pylint disable=protected-access
             prev_addr.network_address._ip):                                      # pylint disable=protected-access
         # Preserve addr's comment, then merge with it.
