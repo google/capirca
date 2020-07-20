d81c5d91b683104d81209b4758f53197b33f9f40
Natalia Maximo <nmaximo@google.com>
2020-07-20 15:53:32 -0400

    fix: fixed indentation on tests

--- tests/lib/cisco_test.py
+++ tests/lib/cisco_test.py
@@ -829,24 +829,24 @@ class CiscoTest(unittest.TestCase):
     self.assertIn('permit udp any any range 1024 65535', str(acl),
                   str(acl))
 
-def testFragments_01(self):
-    """Test policy term using 'fragments' (ref Github issue #187)"""
+  def testFragments_01(self):
+      """Test policy term using 'fragments' (ref Github issue #187)"""
+      self.naming.GetNetAddr.return_value = [nacaddr.IP('10.0.0.0/24')]
+      acl = cisco.Cisco(policy.ParsePolicy(GOOD_HEADER + GOOD_TERM_20,
+                                          self.naming), EXP_INFO)
+      expected = 'permit ip 10.0.0.0 0.0.0.255 10.0.0.0 0.0.0.255 fragments'
+      self.assertIn(expected, str(acl), str(acl))
+
+      self.naming.GetNetAddr.assert_has_calls([mock.call('SOME_HOST'),
+                                              mock.call('SOME_HOST')])
+
+  def testFragments_02(self):
+    """Test policy term using 'is-fragment' (ref Github issue #187)"""
     self.naming.GetNetAddr.return_value = [nacaddr.IP('10.0.0.0/24')]
-    acl = cisco.Cisco(policy.ParsePolicy(GOOD_HEADER + GOOD_TERM_20,
-                                         self.naming), EXP_INFO)
+    acl = cisco.Cisco(policy.ParsePolicy(GOOD_HEADER + GOOD_TERM_22,
+                                        self.naming), EXP_INFO)
     expected = 'permit ip 10.0.0.0 0.0.0.255 10.0.0.0 0.0.0.255 fragments'
-    self.assertIn(expected, str(acl), str(acl))
-
-    self.naming.GetNetAddr.assert_has_calls([mock.call('SOME_HOST'),
-                                             mock.call('SOME_HOST')])
-
-def testFragments_02(self):
-  """Test policy term using 'is-fragment' (ref Github issue #187)"""
-  self.naming.GetNetAddr.return_value = [nacaddr.IP('10.0.0.0/24')]
-  acl = cisco.Cisco(policy.ParsePolicy(GOOD_HEADER + GOOD_TERM_22,
-                                       self.naming), EXP_INFO)
-  expected = 'permit ip 10.0.0.0 0.0.0.255 10.0.0.0 0.0.0.255 fragments'
-  self.assertIn(expected, str(acl))
+    self.assertIn(expected, str(acl))
 
   def testTermDSCPMarker(self):
     self.naming.GetNetAddr.return_value = [nacaddr.IP('10.0.0.0/24')]
