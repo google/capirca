e045c2a090c55550479d9e059042bdb3d6fdb5ac
Axel Fischer <afischer1@godaddy.com>
2020-11-04 17:48:20 +0100

    split repository tags into individual parameters:
    * replace "no-repository-tags" in "target::" header with:
      * "no-repository-revision"
      * "no-repository-date"
      * "no-repository-rid"
    * use them as parameter in AddRepositoryTags():
      * if not present the parameter is True -> print
      * if present the parameter is False -> don't print

--- capirca/lib/juniper.py
+++ capirca/lib/juniper.py
@@ -928,14 +928,20 @@ class Juniper(aclgenerator.ACLGenerator):
       interface_specific = 'not-interface-specific' not in filter_options[1:]
       enable_dsmo = 'enable_dsmo' in filter_options[1:]
       noverbose = 'noverbose' in filter_options[1:]
-      no_repository_tags = 'no-repository-tags' in filter_options[1:]
+      repository_revision = 'no-repository-revision' not in filter_options[1:]
+      repository_date = 'no-repository-date' not in filter_options[1:]
+      repository_rid = 'no-repository-rid' not in filter_options[1:]
 
       if not interface_specific:
         filter_options.remove('not-interface-specific')
       if enable_dsmo:
         filter_options.remove('enable_dsmo')
-      if no_repository_tags:
-        filter_options.remove('no-repository-tags')
+      if not repository_revision:
+        filter_options.remove('no-repository-revision')
+      if not repository_date:
+        filter_options.remove('no-repository-date')
+      if not repository_rid:
+        filter_options.remove('no-repository-rid')
 
       # default to inet4 filters
       filter_type = 'inet'
@@ -978,13 +984,17 @@ class Juniper(aclgenerator.ACLGenerator):
 
       self.juniper_policies.append((header, filter_name,
                                     filter_type, interface_specific,
-                                    no_repository_tags, new_terms))
+                                    repository_revision, repository_date,
+                                    repository_rid, new_terms))
 
   def __str__(self):
     config = Config()
 
-    for (header, filter_name, filter_type, interface_specific, no_repository_tags, terms
-        ) in self.juniper_policies:
+    for (header, filter_name,
+         filter_type, interface_specific,
+         repository_revision,
+         repository_date, repository_rid,
+         terms) in self.juniper_policies:
       # add the header information
       config.Append('firewall {')
       config.Append('family %s {' % filter_type)
@@ -995,10 +1005,9 @@ class Juniper(aclgenerator.ACLGenerator):
       # the tags here when we submit the generator, so we have to trick
       # p4 into not knowing these words.  like taking c-a-n-d-y from a
       # baby.
-      if not no_repository_tags:
-        for line in aclgenerator.AddRepositoryTags('** '):
-          config.Append(line)
-        config.Append('**')
+      for line in aclgenerator.AddRepositoryTags('** ', rid=repository_rid, date=repository_date, revision=repository_revision):
+        config.Append(line)
+      config.Append('**')
 
       for comment in header.comment:
         for line in comment.split('\n'):
