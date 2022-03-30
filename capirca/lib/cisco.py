360c1f002772f9816ce5d75f964bc838d9e7849a
Rajeev Sharma <rdsharma@google.com>
2022-03-30 14:37:21 -0700

    Cisco generator: fix early return when generating verbatim terms.
    Thanks to David Russell (@kaibabbob) for originally contributing this fix. This commit fixes #301 and closes #223.
    
    PiperOrigin-RevId: 438399419

--- capirca/lib/cisco.py
+++ capirca/lib/cisco.py
@@ -121,7 +121,7 @@ class TermStandard:
       for next_verbatim in self.term.verbatim:
         if next_verbatim[0] == self.platform:
           ret_str.append(str(next_verbatim[1]))
-        return '\n'.join(ret_str)
+      return '\n'.join(ret_str)
 
     v4_addresses = [x for x in self.term.address if
                     not isinstance(x, nacaddr.IPv6)]
@@ -503,7 +503,7 @@ class Term(aclgenerator.Term):
       for next_verbatim in self.term.verbatim:
         if next_verbatim[0] == self.platform:
           ret_str.append(str(next_verbatim[1]))
-        return '\n'.join(ret_str)
+      return '\n'.join(ret_str)
 
     # protocol
     if not self.term.protocol:
@@ -826,7 +826,7 @@ class ObjectGroupTerm(Term):
       for next_verbatim in self.term.verbatim:
         if next_verbatim[0] == self.platform:
           ret_str.append(str(next_verbatim[1]))
-        return '\n'.join(ret_str)
+      return '\n'.join(ret_str)
 
     # protocol
     if not self.term.protocol:
